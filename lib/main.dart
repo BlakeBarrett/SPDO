c5b6db3a5ea2e8f7491111118161b863fe6dffe5
Blake Barrett <blake@blakebarrett.net>
2021-07-30 17:26:16 -0700

    Max speed editing works as a popup.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:package_info/package_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'gauges/analog.dart';
+import 'gauges/digital.dart';
 import 'image_picker_utils.dart';
 import 'speedreader.dart';
-import 'gauges/digital.dart';
-import 'gauges/analog.dart';
 
 void main() {
   runApp(SPDO_App());
@@ -75,7 +76,72 @@ class _SpeedometerScaffoldState extends State<SpeedometerScaffold> {
     });
   }
 
-  Widget maxSpeedEditWidget(Widget widgetInQuestion) {
+  Widget backgroundChooserWidget() {
+    return ListTile(
+      title: Row(children: [
+        Icon(Icons.add_a_photo),
+        Spacer(),
+        IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () => ImagePickerUtils.clearImage(),
+        ),
+      ]),
+      onTap: () => ImagePickerUtils.browseForImage(),
+    );
+  }
+
+  Widget unitSelectionWidget() {
+    return SwitchListTile(
+      value: unitsMetric,
+      onChanged: (newValue) => setState(() {
+        unitsMetric = newValue;
+        prefs?.setBool('unitsMetric', newValue);
+      }),
+      title: Text(unitsSubtitle, style: TextStyle(fontWeight: FontWeight.bold)),
+      tileColor: sliderColor,
+      activeColor: sliderColor,
+      activeTrackColor: Colors.grey,
+      inactiveTrackColor: Colors.grey,
+      dense: false,
+      controlAffinity: ListTileControlAffinity.trailing,
+    );
+  }
+
+  Widget digitalSpeedWidget() {
+    return SwitchListTile(
+      value: showDigital,
+      onChanged: (newValue) => setState(() {
+        showDigital = newValue;
+        prefs?.setBool('showDigital', newValue);
+      }),
+      secondary: SvgPicture.asset('assets/numeric.svg'),
+      tileColor: sliderColor,
+      activeColor: sliderColor,
+      activeTrackColor: Colors.grey,
+      inactiveTrackColor: Colors.grey,
+      dense: false,
+      controlAffinity: ListTileControlAffinity.trailing,
+    );
+  }
+
+  Widget analogSpeedWidget() {
+    return SwitchListTile(
+      value: showAnalog,
+      onChanged: (newValue) => setState(() {
+        showAnalog = newValue;
+        prefs?.setBool('showAnalog', newValue);
+      }),
+      secondary: SvgPicture.asset('assets/wiper.svg'),
+      tileColor: sliderColor,
+      activeColor: sliderColor,
+      activeTrackColor: Colors.grey,
+      inactiveTrackColor: Colors.grey,
+      dense: false,
+      controlAffinity: ListTileControlAffinity.trailing,
+    );
+  }
+
+  Widget maxSpeedEditWidget(Widget actionWidget) {
     return Padding(
         padding: EdgeInsets.only(left: 16.0, right: 24.0),
         child: Row(
@@ -88,9 +154,7 @@ class _SpeedometerScaffoldState extends State<SpeedometerScaffold> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(
-                width: iconSize * 2,
-                height: iconSize * 2,
-                child: widgetInQuestion),
+                width: iconSize * 2, height: iconSize * 2, child: actionWidget),
             Text(
               unitsMetric ? "km/h" : "MPH",
               style: TextStyle(fontWeight: FontWeight.bold),
@@ -122,6 +186,40 @@ class _SpeedometerScaffoldState extends State<SpeedometerScaffold> {
     showDialog(context: context, builder: (context) => popup);
   }
 
+  Widget maxSpeedWidget() {
+    if (showAnalog) {
+      return maxSpeedEditWidget(TextButton(
+        autofocus: true,
+        child: Text(maxSpeed.toString()),
+        onPressed: () => showEditPopup(context),
+      ));
+    } else {
+      return Row(children: [
+        Padding(
+          padding: EdgeInsets.all(iconSize.toDouble()),
+        )
+      ]);
+    }
+  }
+
+  Widget aboutWidget() {
+    return AboutListTile(
+      applicationIcon: SvgPicture.asset(
+        'assets/icon.svg',
+        width: iconSize * 2,
+        height: iconSize * 2,
+      ),
+      applicationName: "SPDO",
+      applicationVersion:
+          "${packageInfo?.version} build:${packageInfo?.buildNumber}",
+      aboutBoxChildren: [
+        Center(
+          child: Text("It's a speedometer."),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // Wait until we've actually finished loading everything we need.
@@ -148,85 +246,12 @@ class _SpeedometerScaffoldState extends State<SpeedometerScaffold> {
               color: Colors.black,
               size: iconSize,
             )),
-            ListTile(
-              title: Row(children: [
-                Icon(Icons.add_a_photo),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () => ImagePickerUtils.clearImage(),
-                ),
-              ]),
-              onTap: () => ImagePickerUtils.browseForImage(),
-            ),
-            SwitchListTile(
-              value: showDigital,
-              onChanged: (newValue) => setState(() {
-                showDigital = newValue;
-                prefs?.setBool('showDigital', newValue);
-              }),
-              secondary: SvgPicture.asset('assets/numeric.svg'),
-              tileColor: sliderColor,
-              activeColor: sliderColor,
-              activeTrackColor: Colors.grey,
-              inactiveTrackColor: Colors.grey,
-              dense: false,
-              controlAffinity: ListTileControlAffinity.trailing,
-            ),
-            SwitchListTile(
-              value: unitsMetric,
-              onChanged: (newValue) => setState(() {
-                unitsMetric = newValue;
-                prefs?.setBool('unitsMetric', newValue);
-              }),
-              title: Text(unitsSubtitle,
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              tileColor: sliderColor,
-              activeColor: sliderColor,
-              activeTrackColor: Colors.grey,
-              inactiveTrackColor: Colors.grey,
-              dense: false,
-              controlAffinity: ListTileControlAffinity.trailing,
-            ),
-            SwitchListTile(
-              value: showAnalog,
-              onChanged: (newValue) => setState(() {
-                showAnalog = newValue;
-                prefs?.setBool('showAnalog', newValue);
-              }),
-              secondary: SvgPicture.asset('assets/wiper.svg'),
-              tileColor: sliderColor,
-              activeColor: sliderColor,
-              activeTrackColor: Colors.grey,
-              inactiveTrackColor: Colors.grey,
-              dense: false,
-              controlAffinity: ListTileControlAffinity.trailing,
-            ),
-            if (showAnalog) ...[
-              maxSpeedEditWidget(TextButton(
-                autofocus: true,
-                child: Text(maxSpeed.toString()),
-                onPressed: () => showEditPopup(context),
-              ))
-            ] else ...[
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(iconSize.toDouble()),
-                  )
-                ],
-              ),
-            ],
-            AboutListTile(
-              applicationIcon: SvgPicture.asset(
-                'assets/icon.svg',
-                width: iconSize * 2,
-                height: iconSize * 2,
-              ),
-              applicationName: "SPDO",
-              applicationVersion:
-                  "${packageInfo?.version} build:${packageInfo?.buildNumber}",
-            ),
+            backgroundChooserWidget(),
+            unitSelectionWidget(),
+            digitalSpeedWidget(),
+            analogSpeedWidget(),
+            maxSpeedWidget(),
+            aboutWidget(),
           ],
         ),
       ),
@@ -374,6 +399,22 @@ class _SpeedometerWidgetState extends State<SpeedometerWidget>
     super.dispose();
   }
 
+  Widget settingsDrawerDisclosureIcon() {
+    return Positioned(
+        bottom: 0.0,
+        left: 0.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.black12,
+            ),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     // Loading the background has to happen here, instead of in initState,
@@ -383,15 +424,13 @@ class _SpeedometerWidgetState extends State<SpeedometerWidget>
           _background = value;
         }));
 
-    double _speed = widget.speed;
-    if (_animation != null) {
-      _speed = _animation!.value;
-    }
+    double _speed = (_animation != null) ? _animation!.value : widget.speed;
 
     return Scaffold(
       body: Center(
         child: Stack(children: [
           if (_background != null) ...[Center(child: _background)],
+          settingsDrawerDisclosureIcon(),
           Center(
             child: DigitalGauge(value: widget.display),
           ),
